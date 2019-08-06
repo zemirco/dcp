9952d651b030a2c3ef586c037e4597b9cfc6d4cd
Mirco Zeiss <mirco.zeiss@gmail.com>
2019-08-06 22:08:15 +0200

    use snake case

---
+++ block/all_all_test.go
@@ -0,0 +1,15 @@
+package block
+
+import "testing"
+
+func TestAllAllUnmarshalBinary(t *testing.T) {
+	// todo: write test
+}
+
+func TestAllAllMarshalBinary(t *testing.T) {
+	// todo: write test
+}
+
+func TestAllAllLen(t *testing.T) {
+	// todo: write test
+}
